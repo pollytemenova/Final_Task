6dfb493e970fea131955997b2d9b8ba7082162ac
Polina <gorba4evap@mail.ru>
2023-08-28 22:26:01 +0300

    code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+using System.ComponentModel;
+
+string[] array = { "0", "25", "world", ":-)" };
+int counter = 0;
+for (int i = 0; i < array.Length; i++)
+{
+                        if (array[i].Length <= 3)
+                        {
+                                                counter++;
+                        }
+}
+string[] newArray = new string[counter];
+counter = 0;
+for (int i = 0; i < array.Length; i++)
+{
+                        if (array[i].Length <= 3)
+                        {
+                                                newArray[counter] = array[i];
+                                                counter++;
+                        }
+}
+Console.WriteLine(String.Join(",", newArray));
